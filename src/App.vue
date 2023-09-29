a440afb5f899c1d71ad4e7417417a131a328d7aa
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-29 16:14:25 +0400

    Implemented closing a modal window by clicking outside the modal

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <div class="container">
           <button class="btn btnPrimary" @click="modalFirst = !modalFirst ">Show first modal</button>
           <!-- first modal  -->
-          <Modal title="first modal" v-if="modalFirst"></Modal>
+          <Modal title="first modal" v-if="modalFirst" @close="modalFirst = !modalFirst"></Modal>
         </div>
       </section>
     </div>
@@ -28,32 +28,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
-  font-family: Arial, sans-serif;
-  background-color: #f5f5f5;
-  min-height: 100vh;
-
-  .wrapper-content {
-    padding: 20px;
-
-    section {
-      /* margin: 20px 0; */
-      /* background-color: #ffffff; */
-      /* border-radius: 10px; */
-
-      .container {
-        max-width: 1200px;
-        margin: 0 auto;
-        padding: 20px;
-
-        h1 {
-          color: #333333;
-          font-size: 24px;
-          /* text-align: center; */
-          margin-top: 20px;
-        }
-      }
-    }
-  }
-}
 </style>
