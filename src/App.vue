8bbcf1ed851c77e874cefc3224d96ff5347acf77
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-29 13:41:04 +0400

    add component modal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <!-- header  -->
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <Modal title="first modal"></Modal>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.wrapper {
+  font-family: Arial, sans-serif;
+  background-color: #f5f5f5;
+  min-height: 100vh;
+
+  .wrapper-content {
+    padding: 20px;
+
+    section {
+      /* margin: 20px 0; */
+      /* background-color: #ffffff; */
+      /* border-radius: 10px; */
+
+      .container {
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 20px;
+
+        h1 {
+          color: #333333;
+          font-size: 24px;
+          /* text-align: center; */
+          margin-top: 20px;
+        }
+      }
+    }
+  }
 }
 </style>
