5f7ac942de666dad92d41b938df5d997d5f22d94
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-29 18:05:56 +0400

    reused the modal window, added a basic form with validation

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,25 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
           <!-- first modal  -->
-          <Modal title="first modal" v-show="modalFirst" @close="modalFirst = !modalFirst">
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+          <Modal title="first modal" v-show="modalFirst" @close="modalFirst = false">
             <div slot="body">
               <p>text text text text</p>
               <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well done!</button>
+            </div>
+          </Modal>
+          <!-- second modal  -->
+          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show modal with form</button>
+          <Modal title="modal with show" v-show="modalSecond.show" @close="modalSecond.show = false">
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input type="text" required v-model="modalSecond.name">
+                <label>email</label>
+                <input type="email" required v-model="modalSecond.email">
+                <button class="btn btnPrimary">submit</button>
+              </form>
             </div>
           </Modal>
         </div>
@@ -26,7 +39,23 @@ export default {
   },
   data() {
     return {
-      modalFirst: false
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: '',
+      }
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      })
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
     }
   }
 }
